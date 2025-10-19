d8cb193df44f300e08627a7826432f488278114b
MinhPQ20505 <minhpq2402203@gmail.com>
2025-10-20 01:02:27 +0700

    fix resetpassword

--- src/apis/backend/auth.js
+++ src/apis/backend/auth.js
@@ -15,8 +15,8 @@ const authApi = {
 	forgotPassword: (data) => {
 		const requestBody = {
 			userName: data.username,
-			 domain: process.env.REACT_APP_DOMAIN,
-			//domain:'http://localhost:3000',
+			//  domain: process.env.REACT_APP_DOMAIN,
+			domain:'http://localhost:3000',
 			path: "/reset-password"
 		};
 		console.log('ForgotPassword API - Request Body:', requestBody);
